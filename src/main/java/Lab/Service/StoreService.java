d5ee5834c477068adb50e375a06ddecd8f5b0dc0
RoryEiffe <roryeiffe8@gmail.com>
2023-07-17 10:52:20 -0400

    Added README and added imports.

--- src/main/java/Lab/Service/StoreService.java
+++ src/main/java/Lab/Service/StoreService.java
@@ -3,8 +3,10 @@ package Lab.Service;
 import Lab.Model.Store;
 import Lab.Repository.StoreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 public class StoreService {
     StoreRepository storeRepository;
